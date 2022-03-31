94210eaea6c367eec6e48c1c8e2b797043a18ddf
QIANGPAN <1433858005@qq.com>
2022-03-31 20:33:57 +0800

    2022-3-10-

--- ziji/gf/gfstudent/src/main/java/com/example/gfstudent/controller/StudentController.java
+++ ziji/gf/gfstudent/src/main/java/com/example/gfstudent/controller/StudentController.java
@@ -3,31 +3,72 @@ package com.example.gfstudent.controller;
 import com.example.gfstudent.daomain.Student;
 import com.example.gfstudent.service.StudentService;
 import com.example.gfstudent.service.StudentServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testng.annotations.Test;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 @WebServlet("/a")
 public class StudentController extends HttpServlet {
+    @lombok.SneakyThrows
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        String username=req.getParameter("username");
+        StudentService service=new StudentServiceImpl();
+        ObjectMapper mapper=  new ObjectMapper();
+        //查询所有学生信息
+        ArrayList<Student> list = service.findAll();
+        //json List<Student>转JSON
+        String json =mapper.writeValueAsString(list);
+        System.out.println(json);
+        if(username.equals("1")){
+            System.out.println(111);
+            resp.getWriter().write(json);
+            System.out.println(6666);
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 
     private StudentService service=new StudentServiceImpl();
+    ObjectMapper mapper=  new ObjectMapper();
     //查询所有学生信息
     @Test
-    public void findAll() throws SQLException {
+    public void findAll() throws SQLException, JsonProcessingException {
         ArrayList<Student> list = service.findAll();
-        for (Student stu :list){
-            System.out.println(stu);
-        }
+        //json List<Student>转JSON
+        String json =mapper.writeValueAsString(list);
+        System.out.println(json);
+
+//        for (Student stu :list){
+//
+////            System.out.println(stu);
+//        }
+
     }
     //条件查询，根据id查询学生信息
     @Test
-    public void findById() throws SQLException {
-        Student stu= service.findByid(1);
-        System.out.println(stu);
-
+    public void findById() throws SQLException, JsonProcessingException {
+        Student stu= service.findByid(2);
+//        System.out.println(stu);
+//        User user =new User("张三",23);
+        String json = mapper.writeValueAsString(stu);
+        System.out.println(json);
     }
     //添加学生
     @Test
